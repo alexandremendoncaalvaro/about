370d465477b4b40545742066ed10358152dcee96
alexandremendoncaalvaro <alexandre.alvaro@hotmail.com>
2020-04-15 19:37:57 -0300

    first version

--- js/main.js
+++ js/main.js
@@ -118,20 +118,20 @@
 	/*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
+//    $('.item-wrap a').magnificPopup({
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
+//       type:'inline',
+//       fixedContentPos: false,
+//       removalDelay: 300,
+//       showCloseBtn: false,
+//       mainClass: 'mfp-fade'
 
-   });
+//    });
 
-   $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	e.preventDefault();
-   	$.magnificPopup.close();
-   });
+//    $(document).on('click', '.popup-modal-dismiss', function (e) {
+//    	e.preventDefault();
+//    	$.magnificPopup.close();
+//    });
 
 	
 	/*-----------------------------------------------------*/
